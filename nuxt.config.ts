7aec399e7fa741d918fcd55a73efad5658b6e892
smeaudre.ext <smeaudre.ext@free-mobile.fr>
2024-09-20 15:38:39 +0200

    Init prismic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: true,
+
+  typescript: {
+    typeCheck: true
+  },
+
+  modules: ['@nuxtjs/prismic'],
+  prismic: {
+    endpoint: "https://sandbox-sam.cdn.prismic.io/api/v2"
+  }
 })
